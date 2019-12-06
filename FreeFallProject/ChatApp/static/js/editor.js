9d5d4bfc14fb2eef820c0dba7089511dee2f2f7d
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2019-12-06 23:41:29 +0200

    Есть редактор подробного описания

--- FreeFallProject/ChatApp/static/js/editor.js
+++ FreeFallProject/ChatApp/static/js/editor.js
@@ -1,14 +1,15 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+ClassicEditor
+.create(document.querySelector('#editor'))
+.then(editor => {
+    console.log(editor);
+})
+.catch(error => {
+    console.error(error);
+});
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-} 
+var x = document.createElement("INPUT");
+x.setAttribute("type", "image");
+
+function add_landmark (){
+    
+}
