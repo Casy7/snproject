bf1b6a69756c97269d8403e91d0b1d01bd8f09e3
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2019-12-10 01:22:17 +0200

    Начало работы с треком

--- FreeFallProject/ChatApp/static/js/editor.js
+++ FreeFallProject/ChatApp/static/js/editor.js
@@ -9,7 +9,11 @@ ClassicEditor
 
 var x = document.createElement("INPUT");
 x.setAttribute("type", "image");
+var days = document.getElementsByName("day");
 
-function add_landmark (){
-    
+function add_day (){
+    alert(document.getElementsByName("day")[0]);
+    a_new_day = document.getElementsByName("day")[0].cloneNode(true);
+    alert(document.getElementsByName("day"));
+    document.getElementById("track").appendChild(a_new_day);
 }
