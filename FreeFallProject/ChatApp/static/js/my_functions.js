a00911bf00d35166e8bbb0c21685af2b591020e7
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2020-02-02 21:57:02 +0200

    Валидация даты перенесена в редактор

--- FreeFallProject/ChatApp/static/js/my_functions.js
+++ FreeFallProject/ChatApp/static/js/my_functions.js
@@ -6,3 +6,18 @@ function byId(id){
 function log(value){
     console.log(value);
 }
+
+Date.prototype.toDateInputValue = (function () {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0, 10);
+});
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
+
+function setMinEndDate(){
+    byId('end_day').min = byId('start_day').value;
+}
