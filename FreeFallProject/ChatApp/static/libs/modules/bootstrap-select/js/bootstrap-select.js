55cca678b22877bf3a3d2e4858a838e839890bc8
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2020-02-29 17:51:42 +0200

    Изменён путь к bootstrap-select

--- FreeFallProject/ChatApp/static/libs/modules/bootstrap-select/js/bootstrap-select.js
+++ FreeFallProject/ChatApp/static/libs/modules/bootstrap-select/js/bootstrap-select.js
@@ -1,3 +1,27 @@
+/*!
+ * Bootstrap-select v1.13.9 (https://developer.snapappointments.com/bootstrap-select)
+ *
+ * Copyright 2012-2019 SnapAppointments, LLC
+ * Licensed under MIT (https://github.com/snapappointments/bootstrap-select/blob/master/LICENSE)
+ */
+
+(function (root, factory) {
+  if (root === undefined && window !== undefined) root = window;
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define(["jquery"], function (a0) {
+      return (factory(a0));
+    });
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require("jquery"));
+  } else {
+    factory(root["jQuery"]);
+  }
+}(this, function (jQuery) {
+
 (function ($) {
   'use strict';
 
@@ -3031,3 +3055,7 @@
     })
   });
 })(jQuery);
+
+
+}));
+//# sourceMappingURL=bootstrap-select.js.map
--- FreeFallProject/ChatApp/static/libs/modules/bootstrap-select/js/i18n/defaults-ar_AR.js
+++ FreeFallProject/ChatApp/static/libs/modules/bootstrap-select/js/i18n/defaults-ar_AR.js
@@ -1,4 +1,28 @@
 /*!
+ * Bootstrap-select v1.13.9 (https://developer.snapappointments.com/bootstrap-select)
+ *
+ * Copyright 2012-2019 SnapAppointments, LLC
+ * Licensed under MIT (https://github.com/snapappointments/bootstrap-select/blob/master/LICENSE)
+ */
+
+(function (root, factory) {
+  if (root === undefined && window !== undefined) root = window;
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define(["jquery"], function (a0) {
+      return (factory(a0));
+    });
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require("jquery"));
+  } else {
+    factory(root["jQuery"]);
+  }
+}(this, function (jQuery) {
+
+/*!
  * Translated default messages for bootstrap-select.
  * Locale: AR (Arabic)
  * Author: Yasser Lotfy <y_l@alive.com>
@@ -21,3 +45,7 @@
     multipleSeparator: '، '
   };
 })(jQuery);
+
+
+}));
+//# sourceMappingURL=defaults-ar_AR.js.map
