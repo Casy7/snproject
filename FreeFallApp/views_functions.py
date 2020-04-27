21294eacfc5d29ff16993cb15931ec891db5b816
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2020-04-27 21:38:02 +0300

    Фильтр походов переделан на новый стиль плиток

--- FreeFallApp/views_functions.py
+++ FreeFallApp/views_functions.py
@@ -8,8 +8,9 @@ from FreeFallProject.settings import MEDIA_ROOT, MEDIA_URL, BASE_DIR
 #     new_notification = Notification(user, type_of_notification,)
 #     return 0
 
+
 def check_notifications(user):
-    return list(Notification.objects.filter(user = user))
+    return list(Notification.objects.filter(user=user))
 
 
 def notifications_to_js_format(nt_list):
@@ -17,17 +18,18 @@ def notifications_to_js_format(nt_list):
     for notification in nt_list:
         new_format_nt = []
         new_format_nt.append(notification.type_of_notification)
-        if notification.type_of_notification in ["invite_to_hike","request_for_ptc","user_added_to_hike"]:
-                        
-            user=notification.from_user
+        if notification.type_of_notification in ["invite_to_hike", "request_for_ptc", "user_added_to_hike"]:
+
+            user = notification.from_user
             new_format_nt.append(full_name(user))
             new_format_nt.append(user.id)
             new_format_nt.append(notification.hike.name)
             new_format_nt.append(notification.hike.id)
-            if len(Profile.objects.filter(user = user))>0 and user.profile.avatar.name!='':
+            if len(Profile.objects.filter(user=user)) > 0 and user.profile.avatar.name != '':
                 image = user.profile.avatar
-                with open(os.path.join(BASE_DIR, 'FreeFallApp\\user_files\\')+image.name.replace('/','\\'), "rb") as img_file:
-                    my_string = base64.b64encode(img_file.read()).decode("ASCII")
+                with open(os.path.join(BASE_DIR, 'FreeFallApp\\user_files\\')+image.name.replace('/', '\\'), "rb") as img_file:
+                    my_string = base64.b64encode(
+                        img_file.read()).decode("ASCII")
                 new_format_nt.append(my_string)
                 # str(new_format_nt.append(user.profile.avatar))
             else:
@@ -37,14 +39,10 @@ def notifications_to_js_format(nt_list):
         elif notification.type_of_notification == "simple_text":
 
             result_list.append(new_format_nt)
-                   
 
     return result_list
 
 
-
-
-            
 def full_name(user):
     if user.last_name != '' and user.first_name != '':
         return user.first_name+' '+user.last_name
@@ -74,7 +72,8 @@ def base_context(request, **args):
     context['header'] = 'none'
     context['error'] = 0
     if user.is_anonymous != True:
-        context['notifications'] = notifications_to_js_format(check_notifications(user))
+        context['notifications'] = notifications_to_js_format(
+            check_notifications(user))
         if len(Profile.objects.filter(user=user)) != 0:
 
             context['username'] = user.username
@@ -102,6 +101,7 @@ def base_context(request, **args):
             context[arg] = args[arg]
     # print(context)
     return context
+
 
 def participants_format(participants):
     participants = str(participants)
@@ -146,12 +146,20 @@ def hike_to_json(hike):
     result['name'] = hike.name
     result['description'] = hike.short_description
     result['creator'] = full_name(hike.creator)
-    result['start_date'] = hike.start_date.strftime("%d of %B, %Y")
-    result['end_date'] = hike.end_date.strftime("%d of %B, %Y")
+    result['date'] = beauty_date_interval(hike.start_date, hike.end_date, True)
 
-    if hike.image.name!='':
+
+    result['type'] = hike.type_of_hike
+
+    if hike.difficulty != 'none':
+        result['difficulty'] = hike.difficulty
+    else:
+        result['difficulty'] = 'Без категории'
+
+    if hike.image.name != '':
         try:
-            result['image'] = base64.b64encode(hike.image.read()).decode("ASCII")
+            result['image'] = base64.b64encode(
+                hike.image.read()).decode("ASCII")
         except FileNotFoundError:
             result['image'] = ''
     else:
@@ -161,12 +169,37 @@ def hike_to_json(hike):
 
 
 def cut_keyword(word):
-    if len(word)<=3:
+    # TODO word -> lower(word)
+    if len(word) <= 3:
         return word
-    elif len(word)<=5:
+    elif len(word) <= 5:
         return word[:-1]
-    elif len(word)<=7:
+    elif len(word) <= 7:
         return word[:-2]
     else:
         return word[:-3]
 
+
+def beauty_date_interval(date1: datetime, date2: datetime, show_year=False, show_if_this_year=False):
+    months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+              'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+    result = ''
+    result += str(date1.day) + ' '
+
+    if (date1.day, date1.month, date1.year) == (date2.day, date2.month, date2.year):
+        result += months[date1.month-1]
+    else:
+        if date1.month == date2.month:
+            result += '- '+str(date2.day) + ' ' + months[date1.month-1]
+        else:
+            result += months[date1.month-1]+' - ' + \
+                str(date2.day) + ' '+months[date2.month-1]
+
+    if show_year:
+        if show_if_this_year:
+            result+= ', '+str(date1.year)
+        else:
+            if date1.year != datetime.now().year:
+                result+= ', '+str(date1.year)
+
+    return result
